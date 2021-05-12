5c34c1444412b3a286f48b1e03aaed6bd5a06436
Mihai Eremia <jobs@dani.ro>
2021-05-12 18:02:24 +0100

    New SC

--- react-delegationdashboard/src/config.mainnet.ts
+++ react-delegationdashboard/src/config.mainnet.ts
@@ -37,7 +37,7 @@ export const network: NetworkType = {
   apiAddress: 'https://api.elrond.com',
   gatewayAddress: 'https://gateway.elrond.com',
   explorerAddress: 'https://explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhllllsajxzat',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzhllllsp9wvyl',
 };
 
 const networkSchema = object({
